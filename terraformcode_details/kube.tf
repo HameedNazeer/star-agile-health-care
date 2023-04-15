c5c2b6009f151593ff7a13541cb564407a9a1762
HameedNazeer <122872561+HameedNazeer@users.noreply.github.com>
2023-04-15 18:55:31 +0530

    Update kube.tf

--- terraformcode_details/kube.tf
+++ terraformcode_details/kube.tf
@@ -49,6 +49,12 @@ resource "aws_route_table_association" "a" {
 resource "aws_security_group" "my-sg" {
   vpc_id = aws_vpc.my-vpc.id
   ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
